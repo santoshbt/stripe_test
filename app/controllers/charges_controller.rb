5d037067bc692d2e04d41b74d9ece426994022ae
santoshbt <bt.santosh01@gmail.com>
2018-02-16 18:33:08 +0530

    initial commit

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,21 @@
+class ChargesController < ApplicationController
+    def new
+    end
+
+    def create
+        @amount = 500    ## Amount needs to be in cents
+
+        customer = StripeTool.create_customer(email: params[:stripeEmail], 
+                                              stripe_token: params[:stripeToken]
+                                             )
+
+        charge = StripeTool.create_charge(customer_id: customer.id, 
+                                          amount: @amount,
+                                          description: 'Rails Stripe customer'
+                                          )
+      
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path      
+    end
+end
